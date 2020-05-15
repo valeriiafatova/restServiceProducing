1e0447a009afd9f31fe897629cdd95f8f4bf8cbb
Valeriia Fatova <valeriia_fatova@epam.com>
2020-05-15 13:08:20 +0300

    06 Add Swagger UI

--- src/main/java/com/rest/service/configuration/SwaggerConfiguration.java
+++ src/main/java/com/rest/service/configuration/SwaggerConfiguration.java
@@ -16,7 +16,7 @@ public class SwaggerConfiguration {
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("com.rest.service.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
